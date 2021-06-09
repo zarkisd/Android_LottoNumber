514251beb2090f6113d47898c4021e95d77316a6
zarkisd <xxjunhoxx@naver.com>
2021-06-09 22:03:53 +0900

    20210609

--- app/src/main/java/com/example/lottonumber/MainActivity.kt
+++ app/src/main/java/com/example/lottonumber/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.lottonumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
