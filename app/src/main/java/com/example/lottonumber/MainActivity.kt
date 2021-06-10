e08205cc07b4299e286e758a01aea9c71e5e35e4
zarkisd <xxjunhoxx@naver.com>
2021-06-10 21:45:25 +0900

    20210610 안드로이드 따라가기 겁나벅차네 망할

--- app/src/main/java/com/example/lottonumber/MainActivity.kt
+++ app/src/main/java/com/example/lottonumber/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.lottonumber
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Telephony
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
 
 
