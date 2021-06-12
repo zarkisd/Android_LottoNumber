dd98510fc19d2ac98b3f33bfca30f55bb22d9071
zarkisd <xxjunhoxx@naver.com>
2021-06-12 13:18:19 +0900

    20210612 메인화면완료 (디자인 제외) / 설정 binding true

--- app/src/main/java/com/example/lottonumber/MainActivity.kt
+++ app/src/main/java/com/example/lottonumber/MainActivity.kt
@@ -3,16 +3,33 @@ package com.example.lottonumber
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Telephony
+import android.text.Editable
 import android.widget.TextView
+import android.widget.Toast
+import com.example.lottonumber.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        //여기까지 기본 세팅
 
 
+        binding.btnLogin.setOnClickListener {
+            if (binding.tvId.text.toString() == "aa" && binding.tvPw.text.toString() == "aaa") {
 
+                Toast.makeText(this, "aaaaa", Toast.LENGTH_SHORT).show()
+            }
+
+        }
 
 
     }
 }
+
